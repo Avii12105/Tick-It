47d6fc012d887288c89d6ff2ec9266cdca8c64d5
shlok-angale <bossmonster215@gmail.com>
2026-08-17 00:21:32 +0530

    chore: scaffold django project with dual jinja2/django template config

---
+++ ticketing_platform/config/jinja2.py
@@ -0,0 +1,14 @@
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.urls import reverse
+from jinja2 import Environment
+
+
+def environment(**options):
+    env = Environment(**options)
+    env.globals.update(
+        {
+            "url": reverse,
+            "static": staticfiles_storage.url,
+        }
+    )
+    return env
